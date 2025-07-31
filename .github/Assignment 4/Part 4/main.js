3371a12e408f042c359a255ade6c2cef56fa3432
emmabutera <88450052+emmabutera@users.noreply.github.com>
2025-07-31 21:41:43 +0200

    commit 1
    Added ball counter display to HTML

---
+++ .github/Assignment 4/Part 4/main.js
@@ -0,0 +1,73 @@
+// Set up canvas
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width;
+const height = canvas.height;
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Ball constructor
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+// Draw the ball
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+// Update ball position and bounce off walls
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+// Create multiple balls
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(size, width - size),
+    random(size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`,
+    size
+  );
+  balls.push(ball);
+}
+
+// Animation loop
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
