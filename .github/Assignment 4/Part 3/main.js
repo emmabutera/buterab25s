80782eea62879cca1cd5d92453d5c06b1d999c55
emmabutera <88450052+emmabutera@users.noreply.github.com>
2025-07-31 21:32:35 +0200

    final commit
    Finalized bouncing ball animation and collision logic

--- .github/Assignment 4/Part 3/main.js
+++ .github/Assignment 4/Part 3/main.js
@@ -2,16 +2,14 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-// Set canvas dimensions
 const width = canvas.width;
 const height = canvas.height;
 
-// Function to get random number between two values
 function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// Ball object constructor
+// Ball constructor
 function Ball(x, y, velX, velY, color, size) {
   this.x = x;
   this.y = y;
@@ -21,7 +19,7 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
-// Draw method for Ball
+// Draw the ball
 Ball.prototype.draw = function () {
   ctx.beginPath();
   ctx.fillStyle = this.color;
@@ -29,6 +27,47 @@ Ball.prototype.draw = function () {
   ctx.fill();
 };
 
-// Create one test ball and draw it
-const testBall = new Ball(100, 100, 4, 4, 'red', 30);
-testBall.draw();
+// Update ball position and bounce off walls
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+// Create multiple balls
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(size, width - size),
+    random(size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`,
+    size
+  );
+  balls.push(ball);
+}
+
+// Animation loop
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
