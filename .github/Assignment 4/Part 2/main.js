968d3283b165a5c6d8a89a0f9ea9eb8ca92a2a2c
emmabutera <88450052+emmabutera@users.noreply.github.com>
2025-07-31 20:35:07 +0200

    commit 4
    Added JavaScript to update preview image on thumbnail click

--- .github/Assignment 4/Part 2/main.js
+++ .github/Assignment 4/Part 2/main.js
@@ -1,18 +1,18 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+// List of image file names
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-/* Declaring the array of image filenames */
+// Loop through images and create thumbnails
+for (const filename of imageFilenames) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${filename}`);
+  newImage.setAttribute('alt', `Thumbnail for ${filename}`);
+  thumbBar.appendChild(newImage);
 
-/* Declaring the alternative text for each image file */
-
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
+  newImage.addEventListener('click', () => {
+    displayedImage.setAttribute('src', `images/${filename}`);
+    displayedImage.setAttribute('alt', `Main image: ${filename}`);
+  });
+}
